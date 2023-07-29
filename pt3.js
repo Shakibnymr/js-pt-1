86a1fcf50c8bc62b4ba57979ffd69693a7f2d971
shakibnymr <shakibnymr11@gmail.com>
2023-07-30 03:24:50 +0600

    few files created

---
+++ pt3.js
@@ -0,0 +1,4 @@
+var var1 = 'I am going to be'
+var var2 = 'an awesome web developer'
+var oneLine = var1 + ' ' + var2
+console.log(oneLine)
---
+++ pt4.js
@@ -0,0 +1,4 @@
+var saraMomGivenNum = 119;
+ var divider = 5;
+ var remainder = saraMomGivenNum % divider
+ console.log(remainder)
